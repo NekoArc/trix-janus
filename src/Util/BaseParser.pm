52ff6dd64f39c3c4104764198bc5ead8381ea419
Brenton Scott <admin@trixarian.net>
2012-04-06 03:53:37 +0200

    Ok, coded so it actually WORKS now

--- src/Util/BaseParser.pm
+++ src/Util/BaseParser.pm
@@ -91,7 +91,10 @@ sub cmd2 {
 	my($net,$src) = (shift,shift);
 	my $out = defined $src ? ':'.$net->_out($src).' ' : '';
 	$out .= join ' ', map { $net->_out($_) } @_;
-	$out =~ s/\x03[0-9]{1,2}(,[0-9]{1,2})?|\x02|\x1F|\x16|\x0F//g;
+	# Strips the damn colour codes!
+	$out =~ s/\x03[0-9]{1,2}(,[0-9]{1,2})?//g;
+	$out =~ s/\x02//g;
+	$out =~ s/\x1F//g;
 	$out;
 }
 
