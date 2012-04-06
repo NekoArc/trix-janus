e2ad43d533b9a6577d6d25d15dace8cbcf8030cd
Brenton Scott <admin@trixarian.net>
2012-04-06 03:26:47 +0200

    Added mIRC code stripping

--- src/Util/BaseParser.pm
+++ src/Util/BaseParser.pm
@@ -91,6 +91,7 @@ sub cmd2 {
 	my($net,$src) = (shift,shift);
 	my $out = defined $src ? ':'.$net->_out($src).' ' : '';
 	$out .= join ' ', map { $net->_out($_) } @_;
+	$out =~ s/\x03[0-9]{1,2}(,[0-9]{1,2})?//g;
 	$out;
 }
 
